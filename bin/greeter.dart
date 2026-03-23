5ff316c0dd0c618d9ad255cf5458bf565e62ca81
Jonas N. <jn.jonasneumann@googlemail.com>
2026-03-23 09:15:22 +0100

    mwp, no error handling

--- bin/greeter.dart
+++ bin/greeter.dart
@@ -1,3 +1,21 @@
-void main(List<String> args) {
+import 'dart:io';
 
+void main(List<String> args) {
+  print("Please tell me your first name:");
+  var firstName = stdin.readLineSync();
+  print("Hi $firstName! What is your last name:");
+  var lastName = stdin.readLineSync();
+  print("Would you mind sharing your age aswell?");
+  var age = int.tryParse(stdin.readLineSync()!);
+  print("And finally please reveal your gender!");
+  var gender = stdin.readLineSync();
+
+  print('Good Morning $firstName $lastName');
+}
+
+void validateInput(bool condition, String hintText) {
+  if (!condition) {
+    print(hintText);
+    var newInput = stdin.readLineSync();
+  }
 }
